874f38c381286ace60bf8c919c15c9ee16011d00
ramzihasanov <bd54tkjwr@code.edu.az>
2023-10-29 23:38:02 +0400

    homeworkweek6day1

---
+++ ConsoleApp1/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    [Serializable]
+    public class User:ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string UserName { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("id", Id,typeof(int));
+            info.AddValue("name", Name, typeof(string));
+            info.AddValue("surname", SurName, typeof(string));
+            info.AddValue("username", UserName, typeof(string));
+        }
+
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            Id = info.GetInt32("id");
+            Name = info.GetString("name");
+            SurName = info.GetString("surname");
+            UserName = info.GetString("username");
+
+        }
+        public User()
+        {
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using ConsoleApp1.Models;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //User user = new User();
+            //user.Id = 15;
+            //user.UserName = "remzi.2004";
+            //user.Name = "remzi";
+            //user.SurName = "hesenov";
+            //SerializeToJson(user);
+            User user=DeserializeFromJson();
+            Console.WriteLine(user);
+
+            //    SeryalizeToBinary(user);
+            //    SerializeToXML(user);
+
+            //    User user = DeserializeFromBinary();
+            //User user = DeSerializeFromXML();
+            //Console.WriteLine(user);
+
+
+        }
+
+        public static void SeryalizeToBinary(User user)
+        {
+            string path = "C:\\Users\\hesen\\OneDrive\\İş masası\\User.dat";
+            Stream stream=new FileStream(path,FileMode.Create);
+            BinaryFormatter binaryFormatter=new BinaryFormatter();
+            binaryFormatter.Serialize(stream, user);
+        }
+
+        public static User DeserializeFromBinary()
+        {
+            string path = "C:\\Users\\hesen\\OneDrive\\İş masası\\User.dat";
+            Stream stream = new FileStream(path, FileMode.Open);
+            BinaryFormatter binaryFormatter=new BinaryFormatter();
+            var user =(User)binaryFormatter.Deserialize(stream);
+            return user;
+
+        }
+
+        public static void SerializeToXML(User user)
+        {
+            string path = "C:\\Users\\hesen\\OneDrive\\İş masası\\User.xml";
+            Stream stream = new FileStream(path, FileMode.Create);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(User));
+            xmlSerializer.Serialize(stream, user);
+        }
+
+        public static User DeSerializeFromXML()
+        {
+            string path = "C:\\Users\\hesen\\OneDrive\\İş masası\\User.xml";
+            Stream stream = new FileStream(path, FileMode.Open);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(User));
+            var User =(User)xmlSerializer.Deserialize(stream);
+            return User;
+
+        }
+
+
+        public static void SerializeToJson(User user)
+        {
+            string path = "C:\\Users\\hesen\\OneDrive\\İş masası\\User.json";
+            string json = JsonSerializer.Serialize(user);
+            File.WriteAllText(path, json);
+        }
+
+        public static User DeserializeFromJson()
+        {
+            string path = "C:\\Users\\hesen\\OneDrive\\İş masası\\User.json";
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<User>(json);
+        }
+
+
+       
+    }
+}
